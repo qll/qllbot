5b497fd3643be7e51a47786a241009fa9487161f
qll <mailtoqll@ymail.com>
2011-06-05 23:48:40 +0200

    Fixed usercommands with unicode and gave feedback to usercommand-creation

--- modules/usercommands.py
+++ modules/usercommands.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from settings import *
 from qllbot.Registry import *
-from qllbot.basic_functions import send_message
+from qllbot.basic_functions import send_message, get_username, get_channelname
 
 
 registry = Registry()
@@ -14,35 +14,36 @@ def create_usercommands_tables(param):
 def save(param):
 	''' Saves a usercommand. Example: #save foo bar. !foo -> bar '''
 	if param == '':
-		return 'Empty user command.'
+		return u'Empty user command.'
 	command = param.split(' ', 1)
 	if len(command) < 2:
-		return 'Empty value for command.'
+		return u'Empty value for command.'
 	
 	# check if command already exists (UPDATE), else INSERT
 	c = registry.db.cursor()
-	c.execute('SELECT user FROM usercommands WHERE command = ? AND channel = ?', (command[0].decode('utf-8'), str(registry.cmdinterpreter.current_channel)))
+	c.execute('SELECT user FROM usercommands WHERE command = ? AND channel = ?', (command[0], get_channelname(registry.cmdinterpreter.current_channel)))
 	# c.rowcount does not work as expected, so we use this to determine if there already is a usercommand like that
 	for row in c:
 		c.execute(
 			'UPDATE usercommands SET user = ?, value = ? WHERE command = ? AND channel = ?',
-			(str(registry.cmdinterpreter.current_sender.username), command[1].decode('utf-8'), command[0].decode('utf-8'), str(registry.cmdinterpreter.current_channel))
+			(get_username(registry.cmdinterpreter.current_sender.username), command[1], command[0], get_channelname(registry.cmdinterpreter.current_channel))
 		)
-		return
+		return u'Usercommand updated. Execute with !%s' % command[0]
 	
 	# new command: INSERT
 	registry.db.execute(
 		'INSERT INTO usercommands VALUES (?, ?, ?, ?)',
-		(str(registry.cmdinterpreter.current_sender.username), str(registry.cmdinterpreter.current_channel), command[0].decode('utf-8'), command[1].decode('utf-8'))
+		(get_username(registry.cmdinterpreter.current_sender.username), get_channelname(registry.cmdinterpreter.current_channel), command[0], command[1])
 	)
+	return u'Usercommand saved. Execute with !%s' % command[0]
 
 def interpret_usercommand(param):
 	if param['message'][:1] == USERCOMMANDS_TOKEN:
 		command = param['message'][1:].strip()
 		c = registry.db.cursor()
-		c.execute('SELECT * FROM usercommands WHERE command = ? AND channel = ?', (command.decode('utf-8'), str(param['channel'])))
+		c.execute('SELECT * FROM usercommands WHERE command = ? AND channel = ?', (command, get_channelname(param['channel'])))
 		for row in c:
-			send_message(param['channel'], row[3].encode('utf-8'))
+			send_message(param['channel'], row[3])
 
 
 subscribe('create_tables', create_usercommands_tables)
