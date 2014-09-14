4ba8abef0854699b92bc9ea88089d2ba836c22b2
qll <nicolas.golubovic@rub.de>
2014-09-14 21:49:39 +0200

    Add AI module.

---
+++ modules/ai.py
@@ -0,0 +1,15 @@
+import lib.cmd
+import lib.events
+import lib.irc
+import random
+import settings
+
+
+GREETINGS = ('hi', 'hey ho', 'yoyo!', 'sup\'')
+
+
+@lib.events.subscribe('join')
+def say_hello(bot=None, sender=None, channel=None):
+    """Greets the channel when joining it."""
+    if sender == settings.NICKNAME:
+        bot.send(lib.irc.say(channel, random.choice(GREETINGS)))
