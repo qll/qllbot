37aff1fe7230385ef444eff359e6f73905f2b64e
qll <nicolas.golubovic@rub.de>
2014-09-16 02:03:58 +0200

    Revive the #who command.

--- modules/fun.py
+++ modules/fun.py
@@ -26,19 +26,18 @@ def choice(msg):
     return 'I choose %s' % random.choice(msg.params.split(' '))
 
 
-# @lib.cmd.command()
-# def who(msg):
-#     """#who wants help for this command?"""
-#     if not msg.params:
-#         return
-#     params = ' %s ' % msg.params
-#     replacements = {'me': 'you', 'my': 'your', 'mine': 'yours'}
-#     for a, b in replacements.items():
-#         # TODO: fails at "me you"
-#         params = re.sub(r' (%s|%s)(\?| )' % (a, b),
-#                         lambda m: ' %s%s' % (b if m.group(1) == a else a, m.group(2)),
-#                         params)
-#     params = params.replace('?', random.choice(('!', '.')))
-#     users = Bot().client.channels[get_channel()].users
-#     user = users[choice(list(users.keys()))]
-#     return '{} {}'.format(user, param[1:-1])
+@lib.cmd.command()
+def who(msg):
+    """#who wants help for this command?"""
+    if not msg.params:
+        return
+    params = ' %s ' % msg.params
+    replacements = {'me': 'you', 'my': 'your', 'mine': 'yours'}
+    for a, b in replacements.items():
+        # TODO: fails at "me you"
+        params = re.sub(r' (%s|%s)(\?| )' % (a, b),
+                        lambda m: ' %s%s' % (b if m.group(1) == a else a,
+                                             m.group(2)),
+                        params)
+    params = params.replace('?', random.choice(('!', '.')))
+    return '%s %s' % (random.choice(msg.channel.users), params[1:-1])
