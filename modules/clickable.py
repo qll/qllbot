0f9f1dfdf32af8cd3e0c96539e6572433fc019e9
qll <nicolas.golubovic@rub.de>
2014-09-22 13:02:45 +0200

    Fix subdomain handling of the clickable module.

--- modules/clickable.py
+++ modules/clickable.py
@@ -3,10 +3,13 @@ import lib.irc
 import re
 
 
+LINK = re.compile(r' ((?:[\w\-]+\.)*[\w\-]+\.[a-zA-Z]+\/[^\s]*) ')
+
+
 @lib.event.subscribe('channel_message')
 def make_links_clickable(bot=None, msg=None):
     """Checks every message for unclickable links and makes them clickable."""
-    matches = re.finditer(r' (\w+\.[a-zA-Z]+\/[^\s]*) ', ' %s ' % msg.content)
+    matches = LINK.finditer(' %s ' % msg.content)
     output = ['http://%s' % m.group(1) for m in matches]
     if output:
         # send message back to channel
